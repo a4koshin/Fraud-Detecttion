a5d3e0b2a3b2ea7327a59f674b77baad2419573a
a4koshin <mankajr11@gmail.com>
2025-12-26 01:25:09 +0300

    Pridict folder is created '

---
+++ frontend/app/predict/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import FraudForm from "@/components/FraudForm";
+
+export default function PredictPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <FraudForm />
+    </div>
+  );
+}
